76c57d890c3e011f9c5895774dee5470a2beed97
Pedro Klesse <pedro_klesse@hotmail.com>
2023-04-28 14:28:24 -0300

    feat: second game version 2.0v with JS

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -31,55 +31,48 @@ function playGame(playerSelection, computerSelection){
     remainingLetters2 = computerSelection.slice(1);
     playerSelection = firstLetter1 + remainingLetters1;
     computerSelection = firstLetter2 + remainingLetters2;
-    console.log(computerSelection)
+    
+    console.log("You choose: " + playerSelection)
+    console.log("Computer choose: " + computerSelection)
+    
 
 
     if (playerSelection == "Rock" && computerSelection == "Scissors"){
         console.log("You Win! "+ playerSelection + " beats " + computerSelection);
-        return 1;
+        /*return 1;*/
     }
     else if (playerSelection == "Paper" && computerSelection == "Rock"){
         console.log("You Win! "+ playerSelection + " beats " + computerSelection);
-        return 1;
+        /*return 1;*/
     }
     else if (playerSelection == "Scissors" && computerSelection == "Paper"){
         console.log("You Win! "+ playerSelection + " beats " + computerSelection);
-        return 1;
+        /*return 1;*/
     }
     else if (playerSelection == "Paper" && computerSelection == "Scissors"){
         console.log("You Lose! "+ computerSelection + " beats " + playerSelection);
-        return 0;
+        /*return 0;*/
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper"){
         console.log("You Lose! "+ computerSelection + " beats " + playerSelection);
-        return 0;
+        /*return 0;*/
     }
     else if (playerSelection == "Scissors" && computerSelection == "Rock"){
         console.log("You Lose! "+ computerSelection.charAt(0) + " beats " + playerSelection.charAt(0));
-        return 0;
+        /*return 0;*/
     }
     else {
         console.log("Draw!")
-        return -1;
+        /*return -1;*/
     }
-    return -1;
+    /*return -1;*/
 
 }
 
-function game(){
-    let computerCounter=0;
-    let playerCounter=0;
-    for(let i=0;i<5;i++){
-        result = playGame(getPlayerChoice(),getComputerChoice());
-        if (result == 0)
-            computerCounter++;
-        else if (result ==1)
-            playerCounter++;
-    }
-    if (playerCounter > computerCounter)
-        console.log("Player Wins!")
-    else if (computerCounter > playerCounter)
-        console.log("Computer Wins!")
-    else
-        console.log("Draw!")
-}
+const btns = document.querySelectorAll('#btn');
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', ()=> {
+        playGame(btn.textContent.toLowerCase(), getComputerChoice());
+    });
+});
